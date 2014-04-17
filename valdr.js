a75f1688917682461f06021f0dbee40ccb56f468
Philipp Denzler <philipp.denzler@netcetera.com>
2014-04-17 17:35:06 +0200

    chore(release): v0.1.1

--- valdr.js
+++ valdr.js
@@ -1,5 +1,5 @@
 /**
- * valdr - v0.1.0 - 2014-04-07
+ * valdr - v0.1.1 - 2014-04-17
  * https://github.com/netceteragroup/valdr
  * Copyright (c) 2014 Netcetera AG
  * License: MIT
@@ -471,64 +471,66 @@ angular.module('valdr')
     };
   });
 
-angular.module('valdr')
+/**
+ * This directive adds validation to all input and select fields which are bound to an ngModel and are surrounded
+ * by a valdrType directive.
+ */
+var valdrFormItemDirectiveDefinition =
+  ['valdrEvents', 'valdr', 'valdrUtil', 'valdrClasses', function (valdrEvents, valdr, valdrUtil, valdrClasses) {
+  return  {
+    restrict: 'E',
+    require: ['?^valdrType', '?^ngModel'],
+    link: function (scope, element, attrs, controllers) {
 
-  /**
-   * This directive adds validation to all input fields which are bound to an ngModel and are surrounded
-   * by a valdrType directive.
-   */
-  .directive('input',
-    ['valdrEvents', 'valdr', 'valdrUtil', 'valdrClasses', function (valdrEvents, valdr, valdrUtil, valdrClasses) {
-    return  {
-      restrict: 'E',
-      require: ['?^valdrType', '?^ngModel'],
-      link: function (scope, element, attrs, controllers) {
+      var valdrTypeController = controllers[0],
+        ngModelController = controllers[1],
+        fieldName = attrs.name,
+        parentElement = element.parent();
 
-        var valdrTypeController = controllers[0],
-          ngModelController = controllers[1],
-          fieldName = attrs.name,
-          parentElement = element.parent();
-
-        // Stop right here if this is an <input> that's either not inside of a valdr-type block
-        // or there is no ng-model bound to it.
-        if (!valdrTypeController || !ngModelController) {
-          return;
-        }
-
-        if (valdrUtil.isEmpty(fieldName)) {
-          throw new Error('input is not bound to a field name');
-        }
-
-        var updateClassOnParentElement = function(valid) {
-          parentElement.addClass(valid ? valdrClasses.valid : valdrClasses.invalid);
-          parentElement.removeClass(valid ? valdrClasses.invalid :  valdrClasses.valid);
-        };
-
-        var updateNgModelController = function (validationResult) {
-          ngModelController.$setValidity('valdr', validationResult.valid);
-          ngModelController.valdrViolations = validationResult.violations;
-        };
-
-        var validate = function (value) {
-          var validationResult = valdr.validate(valdrTypeController.getType(), fieldName, value);
-          updateNgModelController(validationResult);
-          updateClassOnParentElement(validationResult.valid);
-          return validationResult.valid ? value : undefined;
-        };
-
-        ngModelController.$parsers.push(validate);
-        ngModelController.$formatters.push(validate);
-
-        scope.$on(valdrEvents.revalidate, function () {
-          validate(ngModelController.$viewValue);
-        });
-
-        element.bind('blur', function () {
-          if (ngModelController.$invalid && ngModelController.$dirty) {
-            parentElement.addClass(valdrClasses.dirtyBlurred);
-          }
-        });
+      // Stop right here if this is a form item that's either not inside of a valdr-type block
+      // or there is no ng-model bound to it.
+      if (!valdrTypeController || !ngModelController) {
+        return;
       }
-    };
-  }]);
+
+      if (valdrUtil.isEmpty(fieldName)) {
+        throw new Error('form element is not bound to a field name');
+      }
+
+      var updateClassOnParentElement = function(valid) {
+        parentElement.addClass(valid ? valdrClasses.valid : valdrClasses.invalid);
+        parentElement.removeClass(valid ? valdrClasses.invalid :  valdrClasses.valid);
+      };
+
+      var updateNgModelController = function (validationResult) {
+        ngModelController.$setValidity('valdr', validationResult.valid);
+        ngModelController.valdrViolations = validationResult.violations;
+      };
+
+      var validate = function (value) {
+        var validationResult = valdr.validate(valdrTypeController.getType(), fieldName, value);
+        updateNgModelController(validationResult);
+        updateClassOnParentElement(validationResult.valid);
+        return validationResult.valid ? value : undefined;
+      };
+
+      ngModelController.$parsers.push(validate);
+      ngModelController.$formatters.push(validate);
+
+      scope.$on(valdrEvents.revalidate, function () {
+        validate(ngModelController.$viewValue);
+      });
+
+      element.bind('blur', function () {
+        if (ngModelController.$invalid && ngModelController.$dirty) {
+          parentElement.addClass(valdrClasses.dirtyBlurred);
+        }
+      });
+    }
+  };
+}];
+
+angular.module('valdr')
+  .directive('input', valdrFormItemDirectiveDefinition)
+  .directive('select', valdrFormItemDirectiveDefinition);
 })(window, document);
